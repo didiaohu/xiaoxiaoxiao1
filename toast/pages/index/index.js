b66968c135fb42df7732a16d016c889aca90b10f
bojuncd <301085344@qq.com>
2017-01-18 19:34:37 +0800

    wx

--- toast/pages/index/index.js
+++ toast/pages/index/index.js
@@ -1,15 +1,21 @@
 // 参考链接：https://github.com/getweapp/weapp-toast
 
+import Promise from 'polyfill';
 // 获取应用实例
 let app = getApp()
 
 Page({
-    data: {},
+    data: {
+        items:{
+            a:123
+        }
+    },
 
     // 仅执行一次，可用于获取、设置数据
     onLoad () {
         //创建可重复使用的WeToast实例，并附加到this上，通过this.wetoast访问
         new app.WeToast()
+        // fetch("http://blog.parryqiu.com").then(function(response){console.log(response)})
     },
 
     onTimeToast () {
