86cfd3933594dcbec747882803596b86f0108234
bojuncd <301085344@qq.com>
2017-03-28 10:43:57 +0800

    添加banner轮播和修改图片

--- mk_rem_flex/js/index.js
+++ mk_rem_flex/js/index.js
@@ -14,4 +14,47 @@
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
+    recalc();
 })(document, window);
+
+
+var innerGroup = $(".innerwraper");
+var leftArrow = $(".left-arrow");
+var rightArrow = $(".right-arrow");
+var spanGroup = $(".pagination span");
+var imgWidth = $(".innerwraper img:first-child").eq(0).width();
+var _index = 0;
+var timer = null;
+var flag = true;
+spanGroup.on("click", function() {
+    //导航切换
+    _index = spanGroup.index($(this));
+    selectPic(_index);
+})
+
+function autoGo(bol) {
+    //自动行走
+    if (bol) {
+        timer = setInterval(go, 3000);
+    }
+}
+autoGo(flag);
+
+function go() {
+    //计时器的函数
+    _index++;
+    selectPic(_index);
+}
+function selectPic(num) {
+    $(".pagination span").eq(num).addClass("active").siblings().removeClass("active");
+    $(".inner").animate({
+        left: -num * imgWidth,
+    }, 1000, function() {
+        //检查是否到最后一张
+        if (_index == innerGroup.length - 1) {
+            _index %= 3;
+            $(".inner").css("left", "0px");
+            $(".pagination span").eq(0).addClass("active").siblings().removeClass("active");
+        }
+    })
+}
